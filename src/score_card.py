80b304475a821de83e9f95b18edc57b23fe52897
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-02-05 09:41:29 +0100

    refactor(score_card): :recycle: Refactorizar los literales a las constantes del tipo enumerado.

--- src/score_card.py
+++ src/score_card.py
@@ -1,3 +1,6 @@
+from src.scoring import Scoring
+
+
 class ScoreCard:
     def __init__(self, score_card):
         self.score_card = score_card
@@ -8,11 +11,11 @@ class ScoreCard:
     def create_frames(self):
         score_card = self.score_card
         position = 0
-        while self.frame_number <= 10:
-            if self.frame_number == 10:
+        while self.frame_number <= Scoring.TEN.value:
+            if self.frame_number == Scoring.TEN.value:
                 self.frames[self.frame_number] = score_card[position:]
                 self.frame_number += 1
-            elif score_card[position] != "X":
+            elif score_card[position] != Scoring.SRIKE.value:
                 self.frames[self.frame_number] = score_card[position : position + 2]
                 position += 2
                 self.frame_number += 1
